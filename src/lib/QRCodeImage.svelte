4c4860a39f9728491167b43fc19645b9e898bb81
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-08-28 23:19:43 -0700

    e

---
+++ src/lib/QRCodeImage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import QRCodeGenerator from 'qrcode';
+
+    let _generatedImgURL:string = "";
+    let imgAlt:string = "QR Code";
+
+    QRCodeGenerator.toDataURL('Hello World').then(url => { _generatedImgURL = url;});
+</script>
+
+<img src={_generatedImgURL} alt={imgAlt}>
